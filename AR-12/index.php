8fd24f6cd5ba5915283d7d7d71ded0eaafd8804e
Franco <gg4545149@gmail.com>
2025-12-10 23:53:54 -0300

    arreglo menor

--- AR-12/index.php
+++ AR-12/index.php
@@ -17,6 +17,7 @@ $userData = $auth->getUserData();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AR-12 Chat - Einherjar Blitz</title>
+    <link rel="icon" href="data:,">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
